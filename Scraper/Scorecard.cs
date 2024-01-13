abc75a91f3fd812e51e14dafd762bb7692d22fc1
shmdtalha <shmdtalha@gmail.com>
2024-01-13 12:08:38 +0500

    Made scorecards from fetched data

---
+++ Scraper/Scorecard.cs
@@ -0,0 +1,90 @@
+﻿using System.Net;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+
+
+namespace CricketScoreScraper.Scraper
+{
+    internal class Scorecard
+    {
+        public string teamA, teamB, scoreA, scoreB, link, innings, status;
+        public string score, wickets, target, coverage, details;
+
+
+        public void SetStringProperty(ref string property, string text)
+        {
+            property = !string.IsNullOrEmpty(text) ? WebUtility.HtmlDecode(text) : "N/A";
+        }
+
+        public void SetTeamA(string text)
+        {
+            SetStringProperty(ref teamA, text);
+        }
+
+        public void SetTeamB(string text)
+        {
+            SetStringProperty(ref teamB, text);
+        }
+
+        public void SetScoreA(string text)
+        {
+            SetStringProperty(ref scoreA, text);
+        }
+
+        public void SetScoreB(string text)
+        {
+            SetStringProperty(ref scoreB, text);
+        }
+
+        public void SetLink(string text)
+        {
+            SetStringProperty(ref link, text);
+        }
+
+        public void SetInnings(string text)
+        {
+            SetStringProperty(ref innings, text);
+        }
+
+        public void SetStatus(string text)
+        {
+            SetStringProperty(ref status, text);
+        }
+
+        public void SetScore(string text)
+        {
+            SetStringProperty(ref score, text);
+        }
+
+        public void SetWickets(string text)
+        {
+            SetStringProperty(ref wickets, text);
+        }
+
+        public void SetTarget(string text)
+        {
+            SetStringProperty(ref target, text);
+        }
+
+        public void SetCoverage(string text)
+        {
+            SetStringProperty(ref coverage, text);
+        }
+
+        public void SetDetails(string text)
+        {
+            SetStringProperty(ref details, text);
+        }
+
+        public void Print()
+        {
+
+            Console.WriteLine($"Details: {details}\n" +
+                $"Coverage: {coverage}\n"+
+                $"Team A: {teamA} - Score A: {scoreA}\n" +
+                $"Team B: {teamB} - Score B: {scoreB}\n" +
+                $"Status: {status}\n" +
+                $"URL: {link}\n\n");
+    
+        }
+    }
+}
--- Scraper/Scores.cs
+++ Scraper/Scores.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using HtmlAgilityPack;
@@ -12,11 +13,7 @@ using static System.Windows.Forms.LinkLabel;
 namespace CricketScoreScraper.Scraper
 {
 
-    struct Scorecard
-    {
-        public string teamA, teamB, link, innings, status;
-        public int score, wickets, target;
-    }
+
 
     internal class Scores
     {
@@ -24,6 +21,7 @@ namespace CricketScoreScraper.Scraper
         private readonly string url;
         private readonly HttpClient client;
         private readonly HtmlAgilityPack.HtmlDocument document;
+        const int batchSize = 15;
 
         public Scores(HttpClient httpClient)
         {
@@ -61,6 +59,7 @@ namespace CricketScoreScraper.Scraper
                 List<HtmlNode> scoreNodes = document.DocumentNode.Descendants("a")
                     .Where(node => node.GetAttributeValue("class", "").Contains(class_scorecard)).ToList();
 
+
                 var tasks = scoreNodes.Select(scoreNode =>
                 {
                     string? url = "https://www.espncricinfo.com" + scoreNode.GetAttributeValue("href", string.Empty);
@@ -68,6 +67,13 @@ namespace CricketScoreScraper.Scraper
                 });
 
                 await Task.WhenAll(tasks);
+
+                Console.WriteLine("Number of Scorecards: " + scorecards.Count);
+
+                foreach(Scorecard scorecard in scorecards)
+                {
+                    scorecard.Print();
+                }
 
                 Console.WriteLine("Done");
             }
@@ -86,17 +92,61 @@ namespace CricketScoreScraper.Scraper
             Scorecard scorecard = new Scorecard();
             await Task.Delay(1000);
 
-            string subPageContent = await client.GetStringAsync(url).ConfigureAwait(false);
             //Load subpage
+            string subPageContent = await client.GetStringAsync(url).ConfigureAwait(false);
             HtmlAgilityPack.HtmlDocument subPage = new HtmlAgilityPack.HtmlDocument();
             subPage.LoadHtml(subPageContent);
 
-            //Fetching details
+            //Making nodes
             string expr_details = "//div[@class='ds-text-tight-m ds-font-regular ds-text-typo-mid3']";
-            HtmlNode? detailsElement = subPage.DocumentNode.SelectSingleNode(expr_details);
-            string? details = detailsElement?.InnerText.Trim();
-            Console.WriteLine(details);
+            HtmlNode? matchDetailsElement = subPage.DocumentNode.SelectSingleNode(expr_details);
+
+            string expr_teamsDetails = "//div[@class='ds-flex ds-flex-col ds-mt-3 md:ds-mt-0 ds-mt-0 ds-mb-1']";
+            HtmlNode? teamsDetailsElement = subPage.DocumentNode?.SelectSingleNode(expr_teamsDetails) ;
+            HtmlNodeCollection? teamDetails = teamsDetailsElement?.SelectNodes(".//div[contains(@class, 'ci-team-score')]");
+
+            string teamA = teamDetails[0].SelectSingleNode(".//span[contains(@class, 'ds-text-tight-l')]").InnerText.Trim();
+            string teamB = teamDetails[1].SelectSingleNode(".//span[contains(@class, 'ds-text-tight-l')]").InnerText.Trim();
+
+            string? scoreTeamA = teamDetails[0].SelectSingleNode(".//strong")?.InnerText.Trim();
+            string? scoreTeamB = teamDetails[1].SelectSingleNode(".//strong")?.InnerText.Trim();
+            string? status = subPage.DocumentNode.SelectSingleNode(".//p[@class = 'ds-text-tight-m ds-font-regular ds-truncate ds-text-typo']/span")?.InnerText?.Trim();
+            string? coverage = subPage.DocumentNode.SelectSingleNode(".//div[@class='ds-px-4 ds-py-3 ds-border-b ds-border-line']//strong")?.InnerText.Trim();
+
+
+
+
+            //Return if an element is null
+            if (matchDetailsElement == null || teamsDetailsElement== null)
+            {
+                Console.WriteLine("An element was null");
+                return;
+            }
+
+            //Fetching match details
+            string details = WebUtility.HtmlDecode(matchDetailsElement.InnerText.Trim());
             
+
+            //Console.WriteLine(details);
+            //Console.WriteLine($"Team A: {teamA}");
+            //Console.WriteLine($"Team B: {teamB}");
+            //Console.WriteLine($"Score A: {scoreTeamA}");
+            //Console.WriteLine($"Score B: {scoreTeamB}");
+            //Console.WriteLine("Coverage: " + coverage);
+            //Console.WriteLine("Status: " + status + "\n\n");
+
+            // Set values using the Scorecard methods
+            scorecard.SetTeamA(teamA);
+            scorecard.SetTeamB(teamB);
+            scorecard.SetScoreA(scoreTeamA);
+            scorecard.SetScoreB(scoreTeamB);
+            scorecard.SetDetails(details);
+            scorecard.SetLink(url);
+            scorecard.SetStatus(status);
+
+            scorecards.Add(scorecard);  
+
+
         }
 
 
